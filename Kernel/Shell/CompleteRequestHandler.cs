c8b5632c4e7d639d6bb3fd5171238217334e72a4
MohamedEihab <mohammedeihabmail+github@gmail.com>
2018-06-08 18:32:01 +0100

    Trying something

--- Kernel/Shell/CompleteRequestHandler.cs
+++ Kernel/Shell/CompleteRequestHandler.cs
@@ -28,6 +28,9 @@ namespace iCSharp.Kernel.Shell
         private volatile AdhocWorkspace workspace;
         private volatile Document documentFile;
 
+        private CancellationTokenSource completionCancellation;
+
+
         public CompleteRequestHandler(ILog logger, IMessageSender messageSender)
         {
             this.logger = logger;
@@ -45,65 +48,104 @@ namespace iCSharp.Kernel.Shell
 
         }
 
+        private async Task<CompletionList> ShowCompletionAsync(int pos, char? triggerChar)
+        {
+            completionCancellation.Cancel();
+            completionCancellation = new CancellationTokenSource();
+            var cancellationToken = completionCancellation.Token;
+
+        //    var completionList = new int pos
+
+            if (triggerChar == null || triggerChar == '.') // || IsAllowedLanguageLetter(triggerChar.Value))
+            {
+                var position = pos;
+                //  var word = GetWord(position);
+
+                var document = documentFile;
+                var completionService = CompletionService.GetService(document);
+
+                var completionList = await Task.Run(async () =>
+                        await completionService.GetCompletionsAsync(document, position, cancellationToken: cancellationToken), cancellationToken);
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                /*  foreach (var completionItem in completionList.Items)
+                  {
+                      completionWindow.CompletionList.CompletionData.Add(new CodeCompletionData(completionItem.DisplayText,
+                          () => GetDescriptionAsync(completionService, document, completionItem), completionItem.Tags));
+                  }*/
+
+                /*   if (triggerChar == null || IsAllowedLanguageLetter(triggerChar.Value))
+                {
+                    completionWindow.StartOffset = word.Item1;
+                    completionWindow.CompletionList.SelectItem(word.Item2);
+                }*/
+                return completionList;
+            }
+            return null;
+        }
+
+
         public void HandleMessage(Message message, RouterSocket serverSocket, PublisherSocket ioPub)
         {
             CompleteRequest completeRequest = JsonSerializer.Deserialize<CompleteRequest>(message.Content);
 
             string code = completeRequest.CodeCells[0];
-          // code = code.Substring(1, code.Length - 2);
 			string line = completeRequest.Line;
-
-            this.logger.Debug("Document Before");
-            this.logger.Debug(documentFile.GetTextAsync().Result);
-
             var sourceText = SourceText.From(code);
-            documentFile = documentFile.WithText(sourceText);
-
-            this.logger.Debug("Document After");
-            this.logger.Debug(documentFile.GetTextAsync().Result);
+            this.logger.Debug(sourceText.ToString());
 
             this.logger.Debug("Position: " + completeRequest.CursorPosition);
 
-            this.logger.Debug("Position: " + completeRequest.CursorPosition);
+            var _list = ShowCompletionAsync(completeRequest.CursorPosition, '.').Result;
 
-            CustomCompletionProvider customCompletionProvider = new CustomCompletionProvider();
-            var completionTriggers = CompletionTrigger.CreateInsertionTrigger('.');
+           // CustomCompletionProvider customCompletionProvider = new CustomCompletionProvider();
+           // var completionTriggers = CompletionTrigger.CreateInsertionTrigger('.');
 
-            var cancellationToken = new CancellationToken();
-            
+            //    var cancellationToken = new CancellationToken();
 
-           CompletionContext completionContext = new CompletionContext(customCompletionProvider, documentFile, completeRequest.CursorPosition, new TextSpan(), CompletionTrigger.Invoke, documentFile.Project.Solution.Workspace.Options, cancellationToken);
-           customCompletionProvider.ProvideCompletionsAsync(completionContext).Wait();
 
-            var _list = GetCompletions(completionContext);//customCompletionProvider.ProvideCompletionsAsync(documentFile, completeRequest.CursorPosition, cancellationToken);
+            //   CompletionContext completionContext = new CompletionContext(customCompletionProvider, documentFile, completeRequest.CursorPosition, new TextSpan(), CompletionTrigger.Invoke, documentFile.Project.Solution.Workspace.Options, cancellationToken);
+            //    customCompletionProvider.ProvideCompletionsAsync(completionContext).Wait();
 
-            var dot = CompletionTrigger.CreateInsertionTrigger('.');
-            var should_trigger = customCompletionProvider.ShouldTriggerCompletion(documentFile.GetTextAsync().Result, completeRequest.CursorPosition, dot, null);
-        
-            this.logger.Debug("Custom Should trigger: " + should_trigger);
+            // var _list = GetCompletions(completionContext);//customCompletionProvider.ProvideCompletionsAsync(documentFile, completeRequest.CursorPosition, cancellationToken);
+
+            //     var dot = CompletionTrigger.CreateInsertionTrigger('.');
+            //      var should_trigger = customCompletionProvider.ShouldTriggerCompletion(documentFile.GetTextAsync().Result, completeRequest.CursorPosition, dot, null);
+
+            //     this.logger.Debug("Custom Should trigger: " + should_trigger);
 
             //completions.
-            var completionService = CompletionService.GetService(documentFile);
+            //        var completionService = CompletionService.GetService(documentFile);
 
-            should_trigger = completionService.ShouldTriggerCompletion(documentFile.GetTextAsync().Result, completeRequest.CursorPosition, dot);
+            //          should_trigger = completionService.ShouldTriggerCompletion(documentFile.GetTextAsync().Result, completeRequest.CursorPosition, dot);
 
-            this.logger.Debug("Normal Should trigger: " + should_trigger);
-            
-       /*     if (should_trigger)
-            {
-                var normal_list = completionService.GetCompletionsAsync(documentFile, completeRequest.CursorPosition, dot);
-                if (normal_list.Result.Items.Length > 0)
-                {
-                    foreach (var item in normal_list.Result.Items)
-                    {
-                        this.logger.Debug("Normal: " + item.DisplayText);
-                    }
-                }
-            }*/
+            //         this.logger.Debug("Normal Should trigger: " + should_trigger);
+
+            /*     if (should_trigger)
+                 {
+                     var normal_list = completionService.GetCompletionsAsync(documentFile, completeRequest.CursorPosition, dot);
+                     if (normal_list.Result.Items.Length > 0)
+                     {
+                         foreach (var item in normal_list.Result.Items)
+                         {
+                             this.logger.Debug("Normal: " + item.DisplayText);
+                         }
+                     }
+                 }*/
 
             List<CompleteReplyMatch> matches_ = new List<CompleteReplyMatch>();
 
-            foreach (var completionItem in _list)
+            /*   foreach (var completionItem in _list)
+                {
+                    CompleteReplyMatch completeReplyMatch = new CompleteReplyMatch()
+                    {
+                        Name = completionItem.DisplayText
+                    };
+                    matches_.Add(completeReplyMatch);
+                }*/
+
+            foreach (var completionItem in _list.Items)
             {
                 CompleteReplyMatch completeReplyMatch = new CompleteReplyMatch()
                 {
@@ -116,28 +158,17 @@ namespace iCSharp.Kernel.Shell
             // this.logger.Debug(ShouldTriggerCompletion(documentFile.GetTextAsync().Result, completeRequest.CursorPosition));
 
 
-
-            /*
-            code = Regex.Replace(code.Substring(1, code.Length - 2), @"\\n", "*");
-            line = line.Substring(1, line.Length - 2);
-
-            int cur_pos = completeRequest.CursorPosition;
-
-            this.logger.Info("cur_pos " + cur_pos);
-
-            line = line.Substring(0, cur_pos); //get substring of code from start to cursor position
-
-            string cursorWord = FindWordToAutoComplete(line);*/
-
-
             CompleteReply completeReply = new CompleteReply()
             {
                 //CursorEnd = 10,
                 Matches = matches_, //ShowCompletionAsync(completeRequest.CursorPosition).Result,
+             //   Matches = ShowCompletionAsync(2).Result,
                 Status = "ok",
                 //CursorStart = 5,
                 // MetaData = null
             };
+
+ 
 
             Message completeReplyMessage = MessageBuilder.CreateMessage(MessageTypeValues.CompleteReply, JsonSerializer.Serialize(completeReply), message.Header);
             this.logger.Info("Sending complete_reply");
@@ -145,23 +176,13 @@ namespace iCSharp.Kernel.Shell
 
         }
 
-        private async Task<List<CompleteReplyMatch>> ShowCompletionAsync(int pos)
+      /*  private async Task<List<CompleteReplyMatch>> ShowCompletionAsync(int pos)
         {
             var completionService = CompletionService.GetService(documentFile);
             var cancellationToken = new CancellationToken();
+
+            var completionList = await completionService.GetCompletionsAsync(documentFile, pos, cancellationToken: cancellationToken);
             List<CompleteReplyMatch> matches_ = new List<CompleteReplyMatch>();
-
-           // if (!(documentFile.GetTextAsync().Result.Length > 0))
-            //{
-             //   return matches_;
-           // }
-             
-            var dot = CompletionTrigger.CreateInsertionTrigger('.');
-            var should_trigger = completionService.ShouldTriggerCompletion(documentFile.GetTextAsync().Result, pos, dot);
-
-            this.logger.Debug("Should trigger: " + should_trigger);
-
-            var completionList = await completionService.GetCompletionsAsync(documentFile, pos+1, cancellationToken: cancellationToken);
 
 
             if (completionList == null)
@@ -181,14 +202,6 @@ namespace iCSharp.Kernel.Shell
 
             return matches_;
         }
-
-        public static IReadOnlyList<CompletionItem> GetCompletions(CompletionContext context)
-        {
-            var property = context.GetType().GetProperty("Items", BindingFlags.Instance | BindingFlags.NonPublic);
-            return (IReadOnlyList<CompletionItem>)property.GetValue(context);
-        }
-
-
 
         public string FindWordToAutoComplete(string line)
         {
@@ -228,7 +241,7 @@ namespace iCSharp.Kernel.Shell
 
             return cursorWord;
 
-        }
+        }*/
 
     }
 }

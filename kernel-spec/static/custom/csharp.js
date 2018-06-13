36a47064207bdc49f40e30e0c109c6ad4716f879
lydzfjzzw <jameszhangzhewei@outlook.com>
2018-06-13 16:10:20 +0100

    Update csharp.js

--- kernel-spec/static/custom/csharp.js
+++ kernel-spec/static/custom/csharp.js
@@ -1,193 +1,168 @@
 CodeMirror.defineMode('csharp', function () {
-    var words = {
-        'abstract': 'keyword',
-        'mohamed': 'keyword',
-        'and': 'keyword',
-        'as': 'keyword',
-        'assert': 'keyword',
-        'base': 'keyword',
-        'begin': 'keyword',
-        'class': 'keyword',
-        'default': 'keyword',
-        'delegate': 'keyword',
-        'do': 'keyword',
-        'done': 'keyword',
-        'downcast': 'keyword',
-        'downto': 'keyword',
-        'elif': 'keyword',
-        'else': 'keyword',
-        'end': 'keyword',
-        'exception': 'keyword',
-        'extern': 'keyword',
-        'false': 'keyword',
-        'finally': 'keyword',
-        'for': 'keyword',
-        'fun': 'keyword',
-        'function': 'keyword',
-        'global': 'keyword',
-        'if': 'keyword',
-        'in': 'keyword',
-        'inherit': 'keyword',
-        'inline': 'keyword',
-        'interface': 'keyword',
-        'internal': 'keyword',
-        'lazy': 'keyword',
-        'let': 'keyword',
-        'let!': 'keyword',
-        'match': 'keyword',
-        'member': 'keyword',
-        'module': 'keyword',
-        'mutable': 'keyword',
-        'namespace': 'keyword',
-        'new': 'keyword',
-        'not': 'keyword',
-        'null': 'keyword',
-        'of': 'keyword',
-        'open': 'keyword',
-        'or': 'keyword',
-        'override': 'keyword',
-        'private': 'keyword',
-        'public': 'keyword',
-        'rec': 'keyword',
-        'return': 'keyword',
-        'return!': 'keyword',
-        'select': 'keyword',
-        'static': 'keyword',
-        'struct': 'keyword',
-        'then': 'keyword',
-        'to': 'keyword',
-        'true': 'keyword',
-        'try': 'keyword',
-        'type': 'keyword',
-        'upcast': 'keyword',
-        'use': 'keyword',
-        'use!': 'keyword',
-        'val': 'keyword',
-        'void': 'keyword',
-        'when': 'keyword',
-        'while': 'keyword',
-        'with': 'keyword',
-        'yield': 'keyword',
-        'yield!': 'keyword',
-        '__SOURCE_DIRECTORY__': 'keyword',
-        'asr': 'keyword',
-        'land': 'keyword',
-        'lor': 'keyword',
-        'lsl': 'keyword',
-        'lsr': 'keyword',
-        'lxor': 'keyword',
-        'mod': 'keyword',
-        'sig': 'keyword',
-        'atomic': 'keyword',
-        'break': 'keyword',
-        'checked': 'keyword',
-        'component': 'keyword',
-        'const': 'keyword',
-        'constraint': 'keyword',
-        'constructor': 'keyword',
-        'continue': 'keyword',
-        'eager': 'keyword',
-        'event': 'keyword',
-        'external': 'keyword',
-        'fixed': 'keyword',
-        'functor': 'keyword',
-        'include': 'keyword',
-        'method': 'keyword',
-        'mixin': 'keyword',
-        'object': 'keyword',
-        'parallel': 'keyword',
-        'process': 'keyword',
-        'protected': 'keyword',
-        'pure': 'keyword',
-        'sealed': 'keyword',
-        'tailcall': 'keyword',
-        'trait': 'keyword',
-        'virtual': 'keyword',
-        'volatile': 'keyword'
-    };
+        var words = {
+            'abstract': 'keyword',
+            'as': 'keyword',
+            'base': 'keyword',
+            'bool': 'builtin',
+            'break': 'keyword',
+            'byte': 'builtin',
+            'case': 'keyword',
+            'catch': 'keyword',
+            'char': 'builtin',
+            'checked': 'keyword',
+            'class': 'keyword',
+            'const': 'keyword',
+            'continue': 'keyword',
+            'decimal': 'builtin',
+            'default': 'keyword',
+            'delegate': 'keyword',
+            'do': 'keyword',
+            'double': 'keyword',
+            'else': 'keyword',
+            'enum': 'keyword',
+            'event': 'keyword',
+            'explicit': 'keyword',
+            'extern': 'keyword',
+            'false': 'keyword',
+            'finally': 'keyword',
+            'fixed': 'keyword',
+            'float': 'builtin',
+            'for': 'keyword',
+            'foreach': 'keyword',
+            'goto': 'keyword',
+            'if': 'keyword',
+            'implicit': 'keyword',
+            'in': 'keyword',
+            'int': 'builtin',
+            'interface': 'keyword',
+            'internal': 'keyword',
+            'is': 'keyword',
+            'lock': 'keyword',
+            'long': 'builtin',
+            'namespace': 'keyword',
+            'new': 'keyword',
+            'null': 'keyword',
+            'object': 'builtin',
+            'operator': 'keyword',
+            'out': 'keyword',
+            'override': 'keyword',
+            'params': 'keyword',
+            'private': 'keyword',
+            'protected': 'keyword',
+            'public': 'keyword',
+            'redonly': 'keyword',
+            'ref': 'keyword',
+            'sbyte': 'builtin',
+            'sealed': 'keyword',
+            'short': 'builtin',
+            'sizeof': 'keyword',
+            'stackalloc': 'keyword',
+            'static': 'keyword',
+            'string': 'builtin',
+            'struct': 'keyword',
+            'switch': 'keyword',
+            'this': 'keyword',
+            'throw': 'keyword',
+            'true': 'keyword',
+            'try': 'keyword',
+            'typeof': 'keyword',
+            'uint': 'builtin',
+            'ulong': 'builtin',
+            'unchecked': 'keyword',
+            'unsafe': 'keyword',
+            'ushort': 'builtin',
+            'using': 'keyword',
+            'var': 'builtin',
+            'virtual': 'keyword',
+            'void': 'keyword',
+            'volatile': 'keyword',
+            'while': 'keyword',
+            
+        };
 
-    function tokenBase(stream, state) {
-        var ch = stream.next();
-
-        if (ch === '"') {
-            state.tokenize = tokenString;
-            return state.tokenize(stream, state);
-        }
-        if (ch === '/') {
-            if (stream.eat('/')) {
-                stream.skipToEnd();
-                return 'comment';
-            }
-        }
-        if (ch === '(') {
-            if (stream.eat('*')) {
-                state.commentLevel++;
-                state.tokenize = tokenComment;
+    
+        function tokenBase(stream, state) {
+            var ch = stream.next();
+    
+            if (ch === '"') {
+                state.tokenize = tokenString;
                 return state.tokenize(stream, state);
             }
-        }
-        if (ch === '~') {
-            stream.eatWhile(/\w/);
-            return 'variable-2';
-        }
-        if (ch === '`') {
-            stream.eatWhile(/\w/);
-            return 'quote';
-        }
-        if (/\d/.test(ch)) {
-            stream.eatWhile(/[\d]/);
-            if (stream.eat('.')) {
+            if (ch === '/') {
+                if (stream.eat('/')) {
+                    stream.skipToEnd();
+                    return 'comment';
+                }
+            }
+            if (ch === '(') {
+                if (stream.eat('*')) {
+                    state.commentLevel++;
+                    state.tokenize = tokenComment;
+                    return state.tokenize(stream, state);
+                }
+            }
+            if (ch === '~') {
+                stream.eatWhile(/\w/);
+                return 'variable-2';
+            }
+            if (ch === '`') {
+                stream.eatWhile(/\w/);
+                return 'quote';
+            }
+            if (/\d/.test(ch)) {
                 stream.eatWhile(/[\d]/);
+                if (stream.eat('.')) {
+                    stream.eatWhile(/[\d]/);
+                }
+                return 'number';
             }
-            return 'number';
-        }
-        if (/[+\-*&%=<>!?|]/.test(ch)) {
-            return 'operator';
-        }
-        stream.eatWhile(/\w/);
-        var cur = stream.current();
-        return words[cur] || 'variable';
-    }
-
-    function tokenString(stream, state) {
-        var next, end = false, escaped = false;
-        while ((next = stream.next()) != null) {
-            if (next === '"' && !escaped) {
-                end = true;
-                break;
+            if (/[+\-*&%=<>!?|]/.test(ch)) {
+                return 'operator';
             }
-            escaped = !escaped && next === '\\';
+            stream.eatWhile(/\w/);
+            var cur = stream.current();
+            return words[cur] || 'variable';
         }
-        if (end && !escaped) {
-            state.tokenize = tokenBase;
+    
+        function tokenString(stream, state) {
+            var next, end = false, escaped = false;
+            while ((next = stream.next()) != null) {
+                if (next === '"' && !escaped) {
+                    end = true;
+                    break;
+                }
+                escaped = !escaped && next === '\\';
+            }
+            if (end && !escaped) {
+                state.tokenize = tokenBase;
+            }
+            return 'string';
         }
-        return 'string';
-    }
-
-    function tokenComment(stream, state) {
-        var prev, next;
-        while (state.commentLevel > 0 && (next = stream.next()) != null) {
-            if (prev === '(' && next === '*') state.commentLevel++;
-            if (prev === '*' && next === ')') state.commentLevel--;
-            prev = next;
+    
+        function tokenComment(stream, state) {
+            var prev, next;
+            while (state.commentLevel > 0 && (next = stream.next()) != null) {
+                if (prev === '(' && next === '*') state.commentLevel++;
+                if (prev === '*' && next === ')') state.commentLevel--;
+                prev = next;
+            }
+            if (state.commentLevel <= 0) {
+                state.tokenize = tokenBase;
+            }
+            return 'comment';
         }
-        if (state.commentLevel <= 0) {
-            state.tokenize = tokenBase;
-        }
-        return 'comment';
-    }
-
-    return {
-        startState: function () { return { tokenize: tokenBase, commentLevel: 0 }; },
-        token: function (stream, state) {
-            if (stream.eatSpace()) return null;
-            return state.tokenize(stream, state);
-        },
-
-        blockCommentStart: "(*",
-        blockCommentEnd: "*)",
-        lineComment: '//'
-    };
-});
+    
+        return {
+            startState: function () { return { tokenize: tokenBase, commentLevel: 0 }; },
+            token: function (stream, state) {
+                if (stream.eatSpace()) return null;
+                return state.tokenize(stream, state);
+            },
+    
+            blockCommentStart: "(*",
+            blockCommentEnd: "*)",
+            lineComment: '//'
+        };
+    });
 
 CodeMirror.defineMIME("text/x-csharp", "csharp");
